7f0862389fb5c186120dc816362864aa2a1d1c8f
PranjalDas <pranjal080015@gmail.com>
2025-01-11 22:58:48 +0530

    beta 1.01

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className='w-screen h-screen text-center flex flex-col items-center justify-center backdrop:blur-sm'>
         <div className='w-1/2 md:w-1/12'>
-          <Image alt='' width={900} height={900} src={'logo.svg'} className='object-cover'/>
+          <Image alt='' width={900} height={900} src={'/Logotextblack.svg'} className='object-cover'/>
         </div>
         <div className='w-full flex flex-col items-center justify-center'>
           {/* <Loader2Icon className='animate-spin'/> */}
