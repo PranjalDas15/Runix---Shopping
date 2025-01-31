a9ced7488490f52bee9db75cfb79c33175810026
PranjalDas <pranjal080015@gmail.com>
2025-01-31 11:00:47 +0530

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,15 @@
-/* eslint-disable */
-
 import type { Metadata } from "next";
 import { Oxanium } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "./storeProvider";
 import ClientLayout from "@/components/ClientLayout";
 
-interface Props {
-  params: { _id: string };
-}
-
 const oxanium = Oxanium({
   subsets: ["latin"],
   variable: "--font-oxanium",
-  weight: ["200", "300", "400", "500", "600","700", "800"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
 const geistSans = localFont({
@@ -45,10 +38,10 @@ export default function RootLayout({
       <body
         className={`${oxanium.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster position="top-right"/>
-          <StoreProvider>
-            <ClientLayout>{children}</ClientLayout>
-          </StoreProvider>
+        <Toaster position="top-right" />
+        <StoreProvider>
+          <ClientLayout>{children}</ClientLayout>
+        </StoreProvider>
       </body>
     </html>
   );
